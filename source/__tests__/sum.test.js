75e0ea32071243108bfeefe4e1aa693f792acdbf
Kailey Wong <kaileyswong@gmail.com>
2024-05-11 19:13:17 -0700

    remove test

--- source/__tests__/sum.test.js
+++ source/__tests__/sum.test.js
@@ -5,7 +5,3 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 2 + 3 to equal 5', () => {
     expect(2 + 3).toBe(5);
 });
-
-test('adds 5 + 5 to equal 10', () => {
-    expect(5 + 5).toBe(10);
-});
