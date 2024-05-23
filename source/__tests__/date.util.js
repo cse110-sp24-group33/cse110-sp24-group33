9a27df1dafa0ddd99bb1e620bca9c6dff910645e
JianingYUE <58084890+JianingYUE@users.noreply.github.com>
2024-05-23 11:33:00 -0700

    Create date.util.js

---
+++ source/__tests__/date.util.js
@@ -0,0 +1,31 @@
+import { 
+  getCurrentDate, 
+  formatDateToYYYYMMDD 
+} from '../scrpts/date.util';
+
+const testDate = new Date(2024, 5, 23);
+global.Date = jest.fn(() => testDate);
+
+decribe('get current Date', () => {
+  it('need to return the current date in "Monthe Day, Year" format', () => {
+    const expectedDate = "May 23, 2024";
+    const result = getCurrentDate();
+    expect(result).toBe(expectedDate)
+  });
+})
+
+describe('formatDateToYYYYMMDD', () => {
+  
+  it('should format a date string to YYYYMMDD', () => {
+    const dateString = "May 23, 2024";
+    const expectedFormattedDate = "20240523";
+    const result = formatDateToYYYYMMDD(dateString);
+    expect(result).toBe(expectedFormattedDate);
+  });
+  it('should return "Invalid Date" for invalid date strings', () => {
+        const invalidDateString = "invalid-date";
+        const result = formatDateToYYYYMMDD(invalidDateString);
+        expect(result).toBe("Invalid Date");
+    });
+});
+  
