9f548cf252901ea8aa28a075c16995bca1f4a943
Kailey Wong <kaileyswong@gmail.com>
2024-05-24 02:47:35 -0700

    add eslint rules

--- source/eslint.config.js
+++ source/eslint.config.js
@@ -3,10 +3,29 @@ import js from "@eslint/js";
 export default [
     js.configs.recommended,
 
-   {
-       rules: {
-           "no-unused-vars": "warn",
-           "no-undef": "warn"
-       }
-   }
+    {
+        "extends": "eslint:recommended",
+
+        "ignorePatterns": ["/*.js", "/docs/**/*.js"],
+
+        env: {
+            browser: true,
+            node: true,
+            es6: true,
+        },
+        parserOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+
+        rules: {
+            "arrow-parens": ["error", "always"],
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+            "indent": ["error", "tab"],
+            "linebreak-style": ["off"],
+            "quotes": ["error", "double"],
+            "semi": ["error", "always"]
+        }
+    }
 ];
