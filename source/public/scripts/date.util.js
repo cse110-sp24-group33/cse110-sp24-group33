b76c5c63d8942d7813af0c77bb0f0c84e5eea10c
Kailey Wong <kaileyswong@gmail.com>
2024-05-24 03:08:50 -0700

    run linter

--- source/public/scripts/date.util.js
+++ source/public/scripts/date.util.js
@@ -2,22 +2,22 @@
 
 // Function to get the current date in "Month Day, Year" format
 function getCurrentDate() {
-    const today = new Date();
-    return today.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    });
+	const today = new Date();
+	return today.toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric"
+	});
 }
 
 // Function to format a date string to YYYYMMDD
 function formatDateToYYYYMMDD(dateString) {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+	const date = new Date(dateString);
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2, "0");
 
-    return `${year}${month}${day}`;
+	return `${year}${month}${day}`;
 }
 
 export { getCurrentDate, formatDateToYYYYMMDD };
