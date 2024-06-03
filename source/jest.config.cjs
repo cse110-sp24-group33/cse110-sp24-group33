9096c30e6c897753df4ee8bc8f82da357bc9c7e7
JacksonYangUCSD <122575102+JacksonYangUCSD@users.noreply.github.com>
2024-06-03 11:38:48 -0700

    Separated jest.config for the github tests to run

--- source/jest.config.cjs
+++ source/jest.config.cjs
@@ -1,18 +1,7 @@
 module.exports = {
-    preset: 'jest-puppeteer',
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest"
     },
-    verbose: true,
-    globals: {
-        // jest-puppeteer global configuration
-        'jest-puppeteer': {
-            launch: {
-                headless: true, // Ensure tests run in headless mode
-                slowMo: 50,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            },
-            browserContext: 'default'
-        }
-    }
+    preset: 'jest-puppeteer',
+    verbose: true
 };
