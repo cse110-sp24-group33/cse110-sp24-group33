4f83b53596d1df3237e0e2b19c5ad0336da98ad3
JacksonYangUCSD <122575102+JacksonYangUCSD@users.noreply.github.com>
2024-06-03 12:07:20 -0700

    testing to see if it runs on headless

--- source/jest-puppeteer.config.cjs
+++ source/jest-puppeteer.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     launch: {
-        headless: true, // Ensure tests run in headless mode
+        headless: false,
         slowMo: 50,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     },
