a7fa617490391b84fa395da7ff1fbe7f69ca269f
Kailey Wong <61530252+kaileywong@users.noreply.github.com>
2024-06-06 03:24:56 -0700

    add jest jsdom and jest-puppeteer compatibility (#66)
    * Create main.index.test.js
    
    * Update and rename sum.test.js to date.util.test.js
    
    * use babel to allow imports for tests
    
    * Update main.index.test.js
    
    * Update main.index.test.js
    
    * Update main.index.test.js
    
    * Update main.index.test.js
    
    * Update main.index.test.js
    
    * add jsdom env for testing and finish tests
    
    * use comment for jsdom testing env
    
    * use comment for e2e env
    
    * jest env configurations
    
    * update match patterns
    
    * update matching paths
    
    * path update
    
    * fixed jest configs with paths
    
    ---------
    
    Co-authored-by: JianingYUE <58084890+JianingYUE@users.noreply.github.com>

--- source/jest.config.cjs
+++ source/jest.config.cjs
@@ -2,6 +2,17 @@ module.exports = {
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest"
     },
-    preset: 'jest-puppeteer',
-    verbose: true
+    verbose: true,
+    projects: [
+        {
+            displayName: "unit",
+            testEnvironment: "jsdom",
+            testMatch: ["**/unit/*.*.test.js"],
+        },
+        {
+            displayName: "e2e",
+            preset: "jest-puppeteer",
+            testMatch: ["**/e2e/*.puppeteer.test.js"],
+        },
+    ],
 };
