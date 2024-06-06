a1fa85b0298cab5ede0751656706a4c763e8f80e
Kailey Wong <61530252+kaileywong@users.noreply.github.com>
2024-06-06 02:48:35 -0700

    Revert "main.index.test.js unit tests (#64)" (#67)
    remove testing conflict

--- source/jest.config.cjs
+++ source/jest.config.cjs
@@ -3,6 +3,5 @@ module.exports = {
         "^.+\\.[t|j]sx?$": "babel-jest"
     },
     preset: 'jest-puppeteer',
-    verbose: true,
-    testEnvironment: "jest-environment-jsdom"
+    verbose: true
 };
